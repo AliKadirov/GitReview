a644e2cb9e9cba11d8e082719e5df27368a32b00
Ali Kodirov <malikodirov@gmail.com>
2022-08-23 10:31:20 -0500

    added ByeWorld.java

---
+++ src/first/ByeWorld.java
@@ -0,0 +1,8 @@
+package first;
+
+public class ByeWorld {
+    public static void main(String[] args) {
+        System.out.println("Maybe 5 weeks if java left");
+        System.out.println("1.5 weeks of git");
+    }
+}
